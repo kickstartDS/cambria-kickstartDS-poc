1f779e44c3b5778e407831e972a902c691c2fed4
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2022-03-17 22:33:01 +0100

    fix: conversion in one direction

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const input = fs.readFileSync("./resources/button-instance.json" || 0, 'utf-8');
 const doc = JSON.parse(input);
 
 // we can (optionally) apply the contents of the changed document to a target document
-// const base = false;
-const base = "./resources/button.schema.json";
+const base = false;
+// const base = "./dist/button-instance.json";
 const targetDoc = base ? JSON.parse(fs.readFileSync(base, 'utf-8')) : {};
+
+const schema = "./resources/button.schema.json";
+const schemaDoc = schema ? JSON.parse(fs.readFileSync(schema, 'utf-8')) : {};
 
 // now load a (yaml) lens definition
 const lensData = fs.readFileSync("./resources/kickstartds-button.lens.yml", 'utf-8');
@@ -20,10 +23,10 @@ if (reverse) {
   lens = cambria.reverseLens(lens);
 }
 
-const updatedSchema = cambria.updateSchema(targetDoc, lens);
+const updatedSchema = cambria.updateSchema(schemaDoc, lens);
 
 // finally, apply the lens to the document, with the schema, onto the target document!
-const newDoc = cambria.applyLensToDoc(lens, doc, base, targetDoc);
-console.log(JSON.stringify(newDoc, null, 2));
+const newDoc = cambria.applyLensToDoc(lens, doc, schemaDoc, targetDoc);
 
+fs.writeFileSync("./dist/button-instance.json", JSON.stringify(newDoc, null, 2));
 fs.writeFileSync("./dist/button.schema.json", JSON.stringify(updatedSchema, null, 2));
